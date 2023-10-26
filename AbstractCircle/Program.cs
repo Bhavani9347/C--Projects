1285535bd4e41a1692db40711afd18c520d3ab47
Bhavani9347 <144900026+Bhavani9347@users.noreply.github.com>
2023-10-26 23:01:01 +0530

    1

---
+++ AbstractCircle/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstractcircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape[] shapes = new Shape[]//array
+            {
+                new Circle(6.0),
+                new Rectangle(2.0, 2.0),
+                new Triangle(2.0, 3.0, 4.0)
+            };
+            double TotalArea = 0.0;
+            double TotalPerimeter = 0.0;
+
+            foreach (var shape in shapes)
+            {
+                TotalArea += shape.CalculatingArea();
+                TotalPerimeter += shape.CalculatingPerimeter();
+            }
+
+            Console.WriteLine($"The total area of shapes is = {TotalArea}");
+            Console.WriteLine($"The total perimeter of shapes is = {TotalPerimeter}");
+
+        }
+    }
+}
---
+++ AbstractCircle/Shape.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstractcircle
+{
+        abstract class Shape//abstarct class
+        {
+            public abstract double CalculatingArea();//abstract method for calculating the total area
+            public abstract double CalculatingPerimeter();//abstract method for calculating the total perimeter
+    }
+
+        class Circle : Shape//inherit
+        {
+            public double Radius { get; }
+
+            public Circle(double radius)//parameterized constructor
+            {
+                Radius = radius;
+            }
+
+            public override double CalculatingArea()
+            {
+                return Math.PI * Radius * Radius;
+            }
+
+            public override double CalculatingPerimeter()
+            {
+                return 2 * Math.PI * Radius;
+            }
+        }
+
+        class Rectangle : Shape//inherit
+        {
+            public double Height { get; }
+            public double Width { get; }
+
+            public Rectangle(double width, double height)//parameterixed constructor
+            {
+                Width = width;
+                Height = height;
+            }
+            public override double CalculatingArea()//method1 for are of rectangle
+            {
+                return Height * Width;
+            }
+
+            public override double CalculatingPerimeter()//method 2 for calculating the perimeter of rectangle
+            {
+                return 2 * (Height + Width);
+            }
+        }
+
+        class Triangle : Shape
+        {
+            public double A { get; }
+            public double B { get; }
+            public double C { get; }
+
+            public Triangle(double a, double b, double c)
+            {
+                A = a;
+                B = b;
+                C = c;
+            }
+        public override double CalculatingArea()//method1 for area of triangle
+        {
+            return 0.5 * A + 0.5 * B;//assuming a as length and b as breadth
+        }
+        public override double CalculatingPerimeter()//method2 for perimeter of traingle
+        {
+            return (A + B + C) / 2;
+        }
+    }
+}
