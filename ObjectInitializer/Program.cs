ec2fd76ed2eb4a9fb562a5d778d3f56d8df891e5
Bhavani9347 <144900026+Bhavani9347@users.noreply.github.com>
2023-10-26 23:23:29 +0530

    19

---
+++ ObjectInitializer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Objectintilalizer
+{
+    internal class Persondetails
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public int age { get; set; }
+
+        static void Main(string[] args)
+        {
+            Persondetails persondetails = new Persondetails
+            {
+                Name = "Shiva",
+                Id = 1234,
+                age = 20
+            };
+            Console.WriteLine(persondetails);
+
+        }
+
+    }
+
+}
