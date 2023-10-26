0948afb043db9ac3b7cd3378259304572f2020a9
Bhavani9347 <144900026+Bhavani9347@users.noreply.github.com>
2023-10-26 23:12:57 +0530

    10

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exceptionhandling
+{
+    internal class Program
+    {
+        int number = 10;
+        public void method()
+        {
+            try
+            {
+                int result = number / 0;
+
+            }
+            catch
+            {
+                Console.WriteLine($"An error has been occured ");
+
+            }
+            finally
+            {
+                Console.WriteLine("The finally block will excute automatically");
+            }
+        }
+      
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.method();
+        }
+    }
+}
