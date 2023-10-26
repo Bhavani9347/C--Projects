9b33be9dea5cc7e3f4b5480a22f3099935ec398c
Bhavani9347 <144900026+Bhavani9347@users.noreply.github.com>
2023-10-26 23:29:37 +0530

    24

---
+++ RegularEXpressions/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Regularexpression
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = "(Mr\\.?|Mrs\\.?|Miss|Ms\\.?)";
+            string[] names = {"Mr.Henry Hunt","Ms.Sara Samuels","Abraham Adams","Ms.Nicole Norris"};
+            foreach (string name in names)
+                Console.WriteLine(Regex.Replace(name,pattern,String.Empty));
+        }
+    }
+}
