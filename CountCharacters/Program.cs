f2ad88c448332951e403a4fa029103256c8184f9
Bhavani9347 <144900026+Bhavani9347@users.noreply.github.com>
2023-10-26 23:11:01 +0530

    9

---
+++ CountCharacters/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace countcharacters
+{
+    internal static class Program
+    {
+        public static int wordCount(this string)
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            string s = "Welcome";
+            int result = s.wordCount();
+            Console.WriteLine(result);
+        }
+    }
+}
