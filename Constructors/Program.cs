a0de488448ccd325704935579c73393198fab639
Bhavani9347 <144900026+Bhavani9347@users.noreply.github.com>
2023-10-26 23:09:48 +0530

    8

---
+++ Constructors/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructors
+{
+    internal class Program
+    {
+       int x,y;
+        //using constructors
+        //parmeterized constructor
+        internal Program(int x,int y)
+        {
+
+             this.x = x;
+             this.y = y;
+           
+        }
+        public void multiply()
+        {
+            int output = x * y;
+            Console.WriteLine($"Multiplication of x and y is {output}");
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program(20, 40);
+            obj.multiply();
+
+
+        }
+    }
+}
