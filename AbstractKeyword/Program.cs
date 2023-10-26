afa077b8124c7651fa20ea7763fa0bf5a795053f
Bhavani9347 <144900026+Bhavani9347@users.noreply.github.com>
2023-10-26 23:01:53 +0530

    2

---
+++ AbstractKeyword/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using static Abstract.abstractchild;
+
+namespace abstractkeyword
+{
+    internal class Programz
+    {
+       static void Main(string[] args)
+        {
+            abstractchild.RoadLines ob = new abstractchild.RoadLines();
+            Console.WriteLine(ob.Mode("Cab"));
+            ob.show();
+        }
+    }
+}
---
+++ AbstractKeyword/abstractchild.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace abstractkeyword
+{
+    internal class abstractchild
+    {
+       
+            internal abstract class TransportMode
+            {
+                public abstract string Mode(string name);  // abstract method signature
+                public void show()
+                {
+                    Console.WriteLine("From Abstract class but a non abstract method");
+                }
+            }
+
+            internal class RoadLines : TransportMode
+            {
+                // implementing abstract method
+                public override string Mode(string name)
+                {
+                    // throw new NotImplementedException();
+                    return string.Format($"Road Line Now using {name}");
+                }
+            }
+        
+    }
+}
