85e8bc20cb12166b9d4ee4765fa1b51ca77be6fc
Bhavani9347 <144900026+Bhavani9347@users.noreply.github.com>
2023-10-26 23:13:54 +0530

    11

---
+++ ExtensionMethod/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace extensionmethod
+{
+    internal static  class Program
+    {
+        public static int WordCount(this string s)//definition of extension
+        {
+                return s.Split(new char[] {' ', '.', ',', '?' }, StringSplitOptions.RemoveEmptyEntries).Length;
+            
+        }
+        public static int Count(this String s)
+        {
+            return s.Length;
+            
+        }
+        static void Main(string[] args)
+        {
+            string s = "The topic is extension method";//assigning the value to the extension method
+            int i = s.WordCount();
+            int length = s.Count();
+            Console.WriteLine(i);
+            Console.WriteLine(length);
+            
+        }
+    }
+}
+
+
+  
