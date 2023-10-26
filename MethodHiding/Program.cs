9279813c119fd77d7f8f04ef9e6f41fbde33f134
Bhavani9347 <144900026+Bhavani9347@users.noreply.github.com>
2023-10-26 23:19:23 +0530

    16

---
+++ MethodHiding/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methodhiding1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            show1 obj= new show1();
+            obj.display();        
+        }
+    }
+}
---
+++ MethodHiding/show.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methodhiding1
+{
+    public class show
+    {
+
+        public void display()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+    /*class show1 : show
+        {
+        public override void display()
+        {
+            Console.WriteLine("Hiding the text");
+        }
+    }*/
+    class show1:show
+
+    {
+        public new void display()
+        {
+            Console.WriteLine("It is overrided derived class method");
+        }
+    }
+
+}
