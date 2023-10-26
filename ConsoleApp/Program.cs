5c6317c4f279b6070eb29d62e26cc6b1ffd284fc
Bhavani9347 <144900026+Bhavani9347@users.noreply.github.com>
2023-10-26 23:06:01 +0530

    4

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Welcome to C#");
+           // Console.Write("It is introduced in 2022");
+            //Console.WriteLine("\n Enter your Employeeid:");
+           // int Employeeid = Convert.ToInt16(Console.ReadLine());
+            //Console.WriteLine($"The employeeid is {Employeeid}");
+            Console.WriteLine("Mention your companyname and location");
+            String companyname = Console.ReadLine();
+            String location = Console.ReadLine();
+            Console.WriteLine($"My company is {companyname}, {location}");
+           // Console.WriteLine($"The location of my company is {location}");
+
+        }
+    }
+}
+
