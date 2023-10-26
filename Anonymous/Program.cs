76d658ac3d720a98f281f32df2cb33e20d699678
Bhavani9347 <144900026+Bhavani9347@users.noreply.github.com>
2023-10-26 23:02:47 +0530

    3

---
+++ Anonymous/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anonymous
+{
+    internal class Anonymous
+    {
+        public void Anonymousdemo()
+        {
+            var s = new 
+            {
+                Amount = 100 ,
+                Message="Have a nice day"
+            };
+            Console.WriteLine(s.Amount + s.Message);
+        }
+        static void Main(string[] args)
+        {
+            Anonymous obj = new Anonymous();
+                obj.Anonymousdemo();
+        }
+    }
+}
