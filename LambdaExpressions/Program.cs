8f21a0b5a36bcc4feb20516eb1d2ddbb14370614
Bhavani9347 <144900026+Bhavani9347@users.noreply.github.com>
2023-10-26 23:18:24 +0530

    15

---
+++ LambdaExpressions/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lambda
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int,int>square= x => x * x;
+            int output = square(7);
+            Console.WriteLine($"The sqaure of 7 is {output}");
+        }
+    }
+}
