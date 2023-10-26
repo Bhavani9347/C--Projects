aca61a9dd894b37519383dc4717a981a67f26bbf
Bhavani9347 <144900026+Bhavani9347@users.noreply.github.com>
2023-10-26 23:25:48 +0530

    21

---
+++ OutKeyword/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Outkeyword
+{
+    internal class Program
+    {
+        public void Main(out int number)
+        {
+            number = 4;//Initializing the value
+        }
+        static void Main(string[] args)
+        {
+            int x;
+            Program program = new Program();
+            program.Main(out x);//just declared 
+            Console.WriteLine(x);//
+
+        }
+    }
+}
