bc566192ec1cf558f17a5866192f31067c19698c
Bhavani9347 <144900026+Bhavani9347@users.noreply.github.com>
2023-10-26 23:06:49 +0530

    5

---
+++ ConsoleAppOutkey/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppoutkey
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Using out
+
+            int i = 0;
+            Console.WriteLine("previous value of integer i:" + i.ToString());
+            string test = outexample(out i);
+            Console.WriteLine("Current value of i :" + i.ToString());
+
+        }
+            public static string outexample(out int id)
+            {
+                id = 1;
+                string returnText = "Next -" + id.ToString();
+                return returnText;
+            }
+    }
+}
